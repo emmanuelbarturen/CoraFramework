9088900d05ab97cea15af8406b43ce8e349259fc
emmanuel <emmanuelbaruren@gmail.com>
2015-08-14 16:25:47 -0500

    Refactor kernel, add login and very basic auth

--- app/views/mensajes/404.blade.php
+++ app/views/mensajes/404.blade.php
@@ -3,24 +3,5 @@
     Opps
     @stop
     @section('contenido')
-    <!-- Content start //-->
-    <main class="site-main">
-        <!-- Intro start //-->
-        <section class="section-404">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12 align-center">
-                        <p class="big">That link is broken.</p>
-                        <p class="font-bold">We can not seem to find /404/</p>
-                        <p class="primary-color">Please use the navigation above or search to find what you are looking for.</p>
-                        <form>
-                            <input type="search" name="s" placeholder="type here...">
-                            <button type="submit" class="biss-btn biss-btn-primary"><i class="fa fa-search"></i></button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- Intro end //-->
-    </main>
+   Pagina no encontrada
 @endsection
